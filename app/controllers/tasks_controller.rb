1b4f3c72816c3ac1b1b96dc27b6d8fab2453c80a
MatsunagaNaoya <mh8scjf6f76ns49r71fb@gmail.com>
2020-03-25 12:53:16 +0000

    c6途中

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,28 @@
 class TasksController < ApplicationController
+
   def index
-    # @tasks = @user.tasks
+    @tasks = Task.all
+  end
+
+  def show
   end
 
   def new
     @task = Task.new
   end
-
-  def edit
+  
+  def create
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスク新規作成しました。"
+      redirect_to user_tasks_url @user
+    else
+      render :new
+    end
   end
 
-  def show
-    @task_id = pasams[:id]
+  def edit
+    
   end
   
 end
