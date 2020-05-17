3698ab2ca0bd4de47567f9dca8a747feb6d4d112
MatsunagaNaoya <mh8scjf6f76ns49r71fb@gmail.com>
2020-05-17 13:06:57 +0000

    C6-4,エラー中

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,19 @@
 class TasksController < ApplicationController
 
   def index
-    @tasks = Task.all
+    @tasks = @user.tasks.all
   end
 
   def show
-    @task_id = params[:id]
+    # @task_id = params[:id]
   end
 
   def new
-    @task = Task.new
+    @task = @user.tasks.new(task_params)
   end
   
   def create
-    @task = @user.tasks.build(task_params)
+    @task = @user.tasks.create(task_params)
     if @task.save
       flash[:success] = "タスク新規作成しました。"
       redirect_to user_tasks_url @user
@@ -26,4 +26,15 @@ class TasksController < ApplicationController
     
   end
   
+    private
+    
+    def set_user
+       @user = User.find(params[:user_id])
+       
+    end
+    
+    def task_params
+      params.require(:task).permit(:title,:note,:user_id)
+
+    end
 end
